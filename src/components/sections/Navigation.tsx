ab5bfa45ff1df138dd3b494beee50a05cff3518d
Tyler St-Amour <tylerstamour1@gmail.com>
2022-10-29 19:29:53 -0400

    Fixed issue with menu height on mobile

--- src/components/sections/Navigation.tsx
+++ src/components/sections/Navigation.tsx
@@ -10,14 +10,16 @@ export default () => {
         <div className={"flex flex-row justify-end gap-16 text-2xl text-gray-400 transition"}>
             {width < 640 ?
                 <div>
-                    <div className={"space-y-2 cursor-pointer"}
-                         style={{ display: visible ? "none" : "inherit" }}
-                         onClick={() => setVisible(true)}
-                         onMouseEnter={() => setBarClass("bg-gray-600")}
-                         onMouseLeave={() => setBarClass("bg-gray-400")}>
-                        <div className={`block h-0.5 w-8 transition duration-200 ${barClass}`}/>
-                        <div className={`block h-0.5 w-8 transition duration-200 ${barClass}`}/>
-                        <div className={`block h-0.5 w-8 transition duration-200 ${barClass}`}/>
+                    <div className={"h-[22px]"}>
+                        <div className={"space-y-2 cursor-pointer"}
+                             style={{ display: visible ? "none" : "inherit" }}
+                             onClick={() => setVisible(true)}
+                             onMouseEnter={() => setBarClass("bg-gray-600")}
+                             onMouseLeave={() => setBarClass("bg-gray-400")}>
+                            <div className={`block h-0.5 w-8 transition duration-200 ${barClass}`}/>
+                            <div className={`block h-0.5 w-8 transition duration-200 ${barClass}`}/>
+                            <div className={`block h-0.5 w-8 transition duration-200 ${barClass}`}/>
+                        </div>
                     </div>
                     <div className={"fixed h-screen bg-gray-100 opacity-95 w-2/5 top-0 right-0 text-gray-500 items-center flex-col gap-6"}
                          style={{display: visible ? "flex" : "none"}}>
