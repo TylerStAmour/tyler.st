a5041553c718adc7c80bc42a633b7cc07c32d56e
Tyler St-Amour <tylerstamour1@gmail.com>
2024-09-12 22:08:25 -0400

    Minor text change to future section

--- src/sections/Future.tsx
+++ src/sections/Future.tsx
@@ -3,9 +3,9 @@ export default () => (
         <p>
             For the future, my goal is to familiarize myself with development on RTOS platforms (specifically Zephyr RTOS).
             I’m hoping to develop some networking applications for RTOS that would improve connectivity for IoT devices
-            running on RTOS. Another goal of mine is to pursue electrical design and hopefully create an ASIC to perform
-            high-speed packet (potential capstone incoming?). This project has a lot of hopefuls, but in general, I want to
-            continue working on networking projects at the low level.
+            running on RTOS. Another goal of mine is to pursue electrical design and hopefully implement high-speed
+            packet processing at the hardware level with an FPGA (potential capstone incoming?). This project has a lot
+            of hopefuls, but in general, I want to continue working on networking projects at the low level.
         </p>
     </div>
 )
